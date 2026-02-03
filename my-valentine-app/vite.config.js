c5ec1e19e9a9026c9ac58a55d5065af7d781683b
sephyrax-lgtm <sephyrax-lgtm@gmail.com>
2026-02-03 11:17:16 +0100

    fix paths

--- my-valentine-app/vite.config.js
+++ my-valentine-app/vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Scam/'  // ← AJOUTE CETTE LIGNE (nom de ton repo)
+  base: '/Scam/'    // ← AJOUTE ÇA
 })
